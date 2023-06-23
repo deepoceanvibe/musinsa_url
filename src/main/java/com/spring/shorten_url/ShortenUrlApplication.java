947385e7ee37375db2b2bb38f479da8f804e501f
deepoceanvibe <d22pocean@gmail.com>
2023-06-23 20:05:12 +0900

    빌드 및 세팅

---
+++ src/main/java/com/spring/shorten_url/ShortenUrlApplication.java
@@ -0,0 +1,13 @@
+package com.spring.shorten_url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/shorten_url/ShortenUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.shorten_url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
